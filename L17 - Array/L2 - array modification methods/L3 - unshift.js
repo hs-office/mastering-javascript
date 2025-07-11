094ee63715f567c77c05f026a1a007975398c480
hs <hakimisaad.hs@gmail.com>
2025-07-11 10:34:47 +0800

    add array-modi-topic

---
+++ L17 - Array/L2 - array modification methods/L3 - unshift.js
@@ -0,0 +1,11 @@
+/*
+ unshift() → Adds item to the start of an array
+ What it does:
+ * It adds one or more items to the beginning of the array
+   and returns the new length of the array.
+ */
+
+let colors = ['green', 'blue']
+colors.unshift('red')
+
+console.log(colors)         // ['red', 'green', 'blue']
---
+++ L17 - Array/L2 - array modification methods/L4 - shift.js
@@ -0,0 +1,11 @@
+/*
+ shift() → Removes the first item from an array
+ What it does:
+ It removes the first element of the array and returns it.
+ */
+
+let colors = ['red', 'green', 'blue']
+let first = colors.shift()
+
+console.log(colors)         // ['green', 'blue']
+console.log(first)          // 'red'
